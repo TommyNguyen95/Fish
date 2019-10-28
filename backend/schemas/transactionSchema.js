6fbef34506a926c2f8e188906cb9b7a233af94be
Hamsho <hamidjade@gmail.com>
2019-10-28 11:26:06 +0100

    Fixed broken schema with correct  and updated gitignore

--- backend/schemas/transactionSchema.js
+++ backend/schemas/transactionSchema.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let transactionSchema = new Schema({
-  to: { type: Schema.Types.ObjectId, ref: 'User'},
-  from: { type: Schema.Types.ObjectId, ref: 'User'},
+  to: { type: Schema.Types.ObjectId, ref: 'User' },
+  from: { type: Schema.Types.ObjectId, ref: 'User' },
   amount: Number,
-  date: Date, default: Date.now,
+  date: { type: Date, default: Date.now },
   message: String,
 })
 
