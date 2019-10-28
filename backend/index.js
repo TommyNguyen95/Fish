17243a8b5b375a94f913583500bf33a3ebb2fb6f
Tommy <tommynguyen@live.se>
2019-10-28 14:36:14 +0100

    added nodemailer

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,46 @@ const cors = require('cors');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const userRoutes = require('./api/userRoutes');
+const nodemailer = require('nodemailer');
+
 
 connectToDb();
 
 const app = express();
+
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+ 
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'apikey',
+      pass: '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'
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  })
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"FI$H" <fishapplication@outlook.com>', // sender address
+    to: 'tommyduynguyen95@gmail.com', // list of receivers
+    subject: 'Welcome to FI$H-APP', // Subject line
+    text: 'BRURRHH', // plain text body
+    // html: '<b>Hello world?</b>' // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+}
+
+main().catch(console.error);
 
 app.use(cors());
 app.use(bodyParser.json());
