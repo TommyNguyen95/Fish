dca163d978a1bb1f4b3298628e5fc3b6aedc8646
Tommy <tommynguyen@live.se>
2019-11-04 10:07:37 +0100

    config for testing

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,5 @@
 // Add your DB name here so you can have your tests for your self (and dont have to see everyone elses tests!)
-const tester = 'hamid';
+const tester = 'Tommy';
 
 const config = {
   db: 'mongodb+srv://team2admin:P4ssw0rd@team2booking-w2vly.mongodb.net/fish-live?retryWrites=true&w=majority',
