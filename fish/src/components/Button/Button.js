0d3811d2f2821b26675f3176ac8baac3d8625260
Pontus Nilsson <p0nta_@hotmail.com>
2019-11-07 16:58:29 +0100

    Added more boilerplate and some design together with simon and delshad

---
+++ fish/src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => {
+  return(
+    <React.fragment></React.fragment>
+  )
+}
+
+export default Button;
---
+++ fish/src/components/Input/StyledInput.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledInput = styled.input`
+  height: 30px;
+  @import url('https://fonts.googleapis.com/css?family=Josefin+Sans:400,400i,700,700i&display=swap');
+  font-family: 'Josefin Sans', sans-serif;
+  width: 55%;
+  border-radius: 3px;
+  border: none;
+  margin-bottom: 10px;
+`
