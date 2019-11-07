7990ecf9017e62aedda0bdf9ecfd822612e28528
Pontus Nilsson <p0nta_@hotmail.com>
2019-11-07 13:35:28 +0100

    Fixed reset CSS as well

--- fish/src/index.js
+++ fish/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import initialState from './state/initialState';
+import 'normalize.css';
 
 const { Provider } = React.createContext();
 
