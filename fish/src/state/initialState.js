53d2594e7e7505d773da2165972bff33b008840e
Pontus Nilsson <p0nta_@hotmail.com>
2019-11-07 12:53:55 +0100

    Boilerplate and architecture for our Fish application

---
+++ fish/src/state/initialState.js
@@ -0,0 +1,8 @@
+export default Test = {
+  exempel1: "",
+  exempel2: 0,
+  exempel3: false,
+  exempel4: []
+}
+
+/* OBS tänk på att värdena kan enbart vara string, nummer, boolean eller array! inte funktioner och dylikt */
