5e2c10fad2a5a35b0873a960f29a71c35c656316
Tommy <tommynguyen@live.se>
2019-11-12 09:45:33 +0100

    Start of verifyView

--- fish/src/App.js
+++ fish/src/App.js
@@ -6,12 +6,14 @@ import {
   Switch
 } from 'react-router-dom';
 import StartPage from './views/StartPage';
+import VerifyPage from './views/VerifyPage';
 
 const App = props => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={StartPage} />
+        <Route exact path="/verify" component={VerifyPage} />
       </Switch>
     </Router>
   )
---
+++ fish/src/views/VerifyPage/VerifyPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const VerifyPage = () => {
+  return (
+    <div>
+      <h1>HEJ</h1>
+    </div>
+  )
+}
+
+export default VerifyPage;
+
