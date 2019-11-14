60584c3d0701c5ce5b6b4a6be0598d349c7c4a6a
SimonPalmgren <simon.palmgren91@gmail.com>
2019-11-14 14:19:29 +0100

    styling and html changes to profile page

--- frontend/src/views/ProfilePage/ProfilePage.js
+++ frontend/src/views/ProfilePage/ProfilePage.js
@@ -5,23 +5,22 @@ import './ProfilePageStyles.scss'
 function ProfilePage() {
   const [userData, setUserData] = useState(true)
   return (
-    <div className="profile-page">
-      <div className="row">
-        <div className="col main-profile">
-          <h2>E-post: Skunk@lunk.1337</h2>
-          {
-            userData ?
-              <React.Fragment>
-                <h2>Tillagda konton</h2>
-                <RelationList></RelationList>
-                <Button text="Skapa barnkonto"></Button>
-                <p className="remove-account">Ta bort ditt konto</p>
-              </React.Fragment> :
-              <Button text="Skapa barnkonto" />
-          }
-        </div>
+    <div>
+      <div className="profile-container">
+        <h3>E-post: Skunk@lunk.1337</h3>
+        {
+          userData ?
+            <React.Fragment>
+              <h3 className="added-accounts-h2">Tillagda konton</h3>
+              <RelationList></RelationList>
+              <Button text="Skapa barnkonto"></Button>
+              <p className="remove-account">Ta bort ditt konto</p>
+            </React.Fragment> :
+            <Button text="Skapa barnkonto" />
+        }
       </div>
     </div>
+
   )
 }
 export default ProfilePage
