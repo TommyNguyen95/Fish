7cec9afd76dad5c60a2389367b0479773543789b
Delshad <Delshadk1@gmail.com>
2019-11-14 14:05:03 +0100

    fixed import

---
+++ frontend/src/views/VerifyPage/VerifyPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BackButton from '../../components/BackButton';
+import {
+    StyledText
+} from './StyledVerifyPage';
+const VerifyPage = () => {
+    return (
+        <div>
+            <BackButton to="/" />
+            <StyledText>
+                Bekräfta din epost genom länken i mejlet skickat till dig!
+      </StyledText>
+        </div>
+    )
+}
+export default VerifyPage;
