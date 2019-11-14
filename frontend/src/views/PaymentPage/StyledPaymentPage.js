de571e912104d3d8432da7bf75ff092de2cfa480
Tommy <tommynguyen@live.se>
2019-11-14 13:57:07 +0100

    re-work

--- frontend/src/views/PaymentPage/StyledPaymentPage.js
+++ frontend/src/views/PaymentPage/StyledPaymentPage.js
@@ -1,17 +1,5 @@
 import styled from 'styled-components';
 
-const StyledForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 80%;
-  max-width: 400px;
-  margin: 0 auto;
-  * {
-    margin: 0.5rem 0;
-  }
-`;
-
 const StyledTextarea = styled.textarea`
   font-family: 'Josefin Sans', sans-serif;
   font-size: 1.1rem;
@@ -23,6 +11,5 @@ const StyledTextarea = styled.textarea`
 `;
 
 export {
-  StyledForm,
   StyledTextarea,
 }
