f7835ede569d1489c1f46cf325f4ef0c61ba59bb
SimonPalmgren <simon.palmgren91@gmail.com>
2019-11-18 11:43:38 +0100

    Added tommys text component to my view + made some changes to the componenent

--- frontend/src/views/ProfilePage/ProfilePage.js
+++ frontend/src/views/ProfilePage/ProfilePage.js
@@ -2,16 +2,17 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Button from '../../components/Button/Button'
 import RelationList from '../../components/RelationList/RelationList'
+import Text from '../../components/Text'
 import './ProfilePageStyles.scss'
 function ProfilePage() {
   const [userData, setUserData] = useState(true)
   return (
     <div className="profile-container">
-      <h3>E-post: Skunk@lunk.1337</h3>
+      <Text text="E-post:" textInput="janne"></Text>
       {
         userData ?
           <React.Fragment>
-            <h3 className="added-accounts-h2">Tillagda konton:</h3>
+            <h3 className="added-accounts-h3">Tillagda konton:</h3>
             <RelationList></RelationList>
             <Link to="/skapa-konto">
               <Button text="Skapa barnkonto" />
