f014d4c263e7b45f3c6728b124d1d3ea4bcdca2e
Kim <kim.johnsson@telia.com>
2019-11-18 13:39:04 +0100

    validation

--- frontend/src/views/CreateAccount/CreateAccount.js
+++ frontend/src/views/CreateAccount/CreateAccount.js
@@ -6,14 +6,22 @@ import Button from '../../components/Button'
 import { createAccountFieldsData } from '../../staticData';
 // import useFetch from '../../helpers/fetch'
 
-const CreateAccount = () => {
+const CreateAccount = (props) => {
   const [createAccountDetails, setCreateAccountDetails] = useState([]);
-  const [kimError, setKimError] = useState(false)
+  const [validateError, setvalidateError] = useState([]);
   const state = useSubContext('loginState')[0];
 
   const renderInputs = () => createAccountFieldsData.map(({ id, name, type, placeholder, capitalize }) => {
     if (name === 'confirmPassword') {
-      return kimError ? <Input bg={"#f8d7da"} key={id} name={name} type={type} placeholder={placeholder} capitalize={capitalize} onChange={checkPassword} /> : <Input key={id} name={name} type={type} placeholder={placeholder} capitalize={capitalize} onChange={checkPassword} />
+      return <Input bg={validateError.confirmPassword} key={id} name={name} type={type} placeholder={placeholder} capitalize={capitalize} onChange={checkPassword} />
+    } else if (name === 'lastname') {
+      return <Input bg={validateError.lastname} key={id} name={name} type={type} placeholder={placeholder} capitalize={capitalize} onChange={handleInputs} />
+    } else if (name === 'firstname') {
+      return <Input bg={validateError.firstname} key={id} name={name} type={type} placeholder={placeholder} capitalize={capitalize} onChange={handleInputs} />
+    } else if (name === 'ssn') {
+      return <Input bg={validateError.ssn} key={id} name={name} type={type} placeholder={placeholder} capitalize={capitalize} onChange={handleInputs} />
+    } else if (name === 'username') {
+      return <Input bg={validateError.username} key={id} name={name} type={type} placeholder={placeholder} capitalize={capitalize} onChange={handleInputs} />
     } else {
       return <Input key={id} name={name} type={type} placeholder={placeholder} capitalize={capitalize} onChange={handleInputs} />
     }
@@ -21,7 +29,7 @@ const CreateAccount = () => {
 
   const createAccount = async (e) => {
     e.preventDefault()
-    if (createAccountDetails.password === createAccountDetails.confirmPassword && createAccountDetails.confirmPassword) {
+    if (createAccountDetails.password === createAccountDetails.confirmPassword && createAccountDetails.confirmPassword && createAccountDetails.lastname && createAccountDetails.firstname && createAccountDetails.ssn) {
       await fetch(`${state.apiEndpoint}/api/user`,
         {
           method: "POST",
@@ -30,12 +38,22 @@ const CreateAccount = () => {
         })
         .then(res => { console.log(res) })
         .catch(res => { console.log(res) })
-    } else if (!createAccountDetails.confirmPassword || createAccountDetails.confirmPassword !== createAccountDetails.password) {
-      setKimError(true)
+      props.history.goBack()
+    } if (!createAccountDetails.ssn) {
+      setvalidateError({ ...validateError, ssn: "#f8d7da" })
+    } if (!createAccountDetails.confirmPassword || createAccountDetails.confirmPassword !== createAccountDetails.password) {
+      setvalidateError({ ...validateError, confirmPassword: "#f8d7da" })
+    } if (!createAccountDetails.username) {
+      setvalidateError({ ...validateError, username: "#f8d7da" })
+    } if (!createAccountDetails.lastname) {
+      setvalidateError({ ...validateError, lastname: "#f8d7da" })
+    } if (!createAccountDetails.firstname) {
+      setvalidateError({ ...validateError, firstname: "#f8d7da" })
     }
   }
 
   const handleInputs = (e) => {
+    setvalidateError({ ...validateError, [e.target.getAttribute('name')]: "#fff" })
     setCreateAccountDetails({ ...createAccountDetails, [e.target.getAttribute('name')]: e.target.value })
   }
 
