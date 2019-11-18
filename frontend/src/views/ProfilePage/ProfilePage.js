2aef25142a9a060f4fafc6437ef5554d9a4e3e7e
SimonPalmgren <simon.palmgren91@gmail.com>
2019-11-18 20:53:54 +0100

    made render cleaner by dividing some code

--- frontend/src/views/ProfilePage/ProfilePage.js
+++ frontend/src/views/ProfilePage/ProfilePage.js
@@ -13,7 +13,6 @@ const ProfilePage = () => {
   const state = useSubContext('userState')[0];
   const [data, setData] = useState([])
   let relations = data.relations
-  relations = null
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,24 +24,26 @@ const ProfilePage = () => {
     fetchData();
   }, []);
 
+  const renderChildsAccount = () => {
+    if (relations) {
+      return (
+        <React.Fragment>
+          <h3 className="added-accounts-h3">Tillagda konton:</h3>
+          {relations.map(child =>
+            <Link to={`/barn-profil/${child._id}`} key={child._id}>
+              <RelationList email={child.username} />
+            </Link>
+          )}
+        </React.Fragment>
+      )
+    }
+  }
+
   return (
     <div className="profile-container">
       <Text text={data.firstname} textInput={data.lastname} />
       <Text text="E-post:" textInput={data.username} />
-      {
-        relations ?
-          <React.Fragment>
-            <h3 className="added-accounts-h3">Tillagda konton:</h3>
-            {relations.map(child =>
-              <Link to={`/barn-profil/${child._id}`} key={child._id}>
-                <RelationList email={child.username} />
-              </Link>
-            )}
-          </React.Fragment>
-          :
-          <React.Fragment>
-          </React.Fragment>
-      }
+      {renderChildsAccount()}
       <Link to="/skapa-konto">
         <Button text="Skapa barnkonto" />
       </Link>
