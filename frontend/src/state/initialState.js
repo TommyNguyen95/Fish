bd390f0a6ff887b54225e17376002ff7badda354
Tommy <tommynguyen@live.se>
2019-11-19 14:51:13 +0100

    push for hamid

--- frontend/src/state/initialState.js
+++ frontend/src/state/initialState.js
@@ -1,5 +1,6 @@
 export default {
   apiEndpoint: (process.env.NODE_ENV === 'production' ? 'https://api.getfish.se' : 'http://localhost:3001'),
+  showLogo: true,
   loginState: {
     username: '',
     password: '',
