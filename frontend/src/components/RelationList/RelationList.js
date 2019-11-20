56645bc21f6d90912d0377d96336426f2e96b018
SimonPalmgren <simon.palmgren91@gmail.com>
2019-11-20 12:35:37 +0100

    Trying to fix state when deleing a child

--- frontend/src/components/RelationList/RelationList.js
+++ frontend/src/components/RelationList/RelationList.js
@@ -11,8 +11,15 @@ const RelationList = ({ email, id }) => {
   const deleteChildOnClick = () => {
     axios.delete(`${state.apiEndpoint}/api/user/${id}`)
       .then(response => {
-        console.log(response);
-        console.log(response.data);
+        let childToRemove
+        state.loginState.relations.filter((item, index) => {
+          if (item._id === response.data._id) {
+            childToRemove = index
+            return true
+          }
+        })
+        state.loginState.relations.splice(childToRemove, 1)
+        dispatch({ type: "RESET_STATE", value: state.loginState })
       })
   }
 
